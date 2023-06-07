12db65dd192e40ac4d0b228996f3c1a9a94d1e96
Igor Sheverda <ktgtoit@gmail.com>
2023-06-07 09:24:49 +0300

    Initial Commit

---
+++ NewsApp_Spaceflightnewsapi_net/NewsApp_Spaceflightnewsapi_netApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp_Spaceflightnewsapi_netApp.swift
+//  NewsApp_Spaceflightnewsapi_net
+//
+//  Created by user on 07.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp_Spaceflightnewsapi_netApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
